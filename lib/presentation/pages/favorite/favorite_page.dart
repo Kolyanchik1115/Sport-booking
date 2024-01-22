29b3a420052c1efebc7ae01faf739aa574a6862e
Sprat <105858796+Kolyanchik1115@users.noreply.github.com>
2024-01-22 20:17:02 +0200

    feat: add booking page

--- lib/presentation/pages/favorite/favorite_page.dart
+++ lib/presentation/pages/favorite/favorite_page.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
-
+import 'package:sport_app/presentation/pages/favorite/fas.dart';
+import 'package:sport_app/presentation/widgets/empty_layout.dart';
 
 class FavoritePage extends StatelessWidget {
   const FavoritePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder(color: Colors.black);
+    return CustomWidget();
   }
 }
+
