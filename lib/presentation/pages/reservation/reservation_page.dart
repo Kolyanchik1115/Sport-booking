beec775b289076f2a54800ecb53e1812d2ff7191
Sprat <105858796+Kolyanchik1115@users.noreply.github.com>
2024-05-09 15:48:13 +0300

    feat: add reservation page

--- lib/presentation/pages/reservation/reservation_page.dart
+++ lib/presentation/pages/reservation/reservation_page.dart
@@ -1,11 +1,172 @@
 import 'package:flutter/material.dart';
+import 'package:sport_app/features/additional_pages/presentation/widgets/scaffold_with_app_bar.dart';
 
 class ReservationPage extends StatelessWidget {
-  const ReservationPage({super.key});
+  const ReservationPage({
+    super.key,
+  });
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder(color: Colors.pink);
+    return ScaffoldWithAppBar(
+      appBarTitle: "My reservations",
+      canPop: false,
+      child: CustomListWidget(),
+    );
   }
 }
 
+class CustomListWidget extends StatelessWidget {
+  final List<Map<String, dynamic>> items = [
+    {
+      'image':
+      'https://images.unsplash.com/photo-1481349518771-20055b2a7b24?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8cmFuZG9tfGVufDB8fDB8fHww',
+      'title': 'Спортивный клуб "Энергетик"',
+      'number': '1234',
+      'date': '01.01.2024',
+      'time': '19:30',
+      'status': 'оплачен',
+    },
+    {
+      'image':
+      'https://images.unsplash.com/photo-1481349518771-20055b2a7b24?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8cmFuZG9tfGVufDB8fDB8fHww',
+      'title': 'Спортивный клуб "Титан"',
+      'number': '5678',
+      'date': '02.01.2024',
+      'time': '20:30',
+      'status': 'в ожидании',
+    },
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: items.length,
+      itemBuilder: (context, index) {
+        final item = items[index];
+        Color statusColor;
+        switch (item['status']) {
+          case 'оплачен':
+            statusColor = Colors.green;
+            break;
+          case 'в ожидании':
+            statusColor = Colors.orange;
+            break;
+          case 'отменен':
+            statusColor = Colors.red;
+            break;
+          default:
+            statusColor = Colors.grey;
+            break;
+        }
+        return Padding(
+          padding: const EdgeInsets.symmetric(vertical: 10.0, horizontal: 10.0),
+          child: Stack(
+            alignment: Alignment.topRight,
+            children: [
+              Row(
+                children: [
+                  ClipRRect(
+                    borderRadius: BorderRadius.circular(8.0),
+                    child: Image.network(item['image'], width: 160, height: 120, fit: BoxFit.cover),
+                  ),
+                  const SizedBox(width: 16.0),
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Positioned(
+                              top: 8,
+                              left: 8,
+                              child: Row(
+                                children: [
+                                  Container(
+                                    width: 12,
+                                    height: 12,
+                                    decoration: BoxDecoration(
+                                      color: statusColor,
+                                      borderRadius: BorderRadius.circular(6),
+                                    ),
+                                  ),
+                                  const SizedBox(width: 8),
+                                  Text(
+                                    item['status'],
+                                    style: TextStyle(
+                                      color: statusColor,
+                                      fontSize: 12,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                            Text(
+                              '№ ${item['number']}',
+                              style: Theme.of(context)
+                                  .textTheme
+                                  .titleLarge
+                                  ?.copyWith(color: Theme.of(context).colorScheme.onBackground),
+                            ),
+                          ],
+                        ),
+                        const SizedBox(height: 20),
+                        Text(item['title'], style: Theme.of(context).textTheme.headline6),
+                        const SizedBox(height: 10),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                const Text(
+                                  'Дата:',
+                                  style: TextStyle(
+                                    fontSize: 12,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                                Text(
+                                  item['date'],
+                                  style: const TextStyle(
+                                    fontSize: 12,
+                                  ),
+                                ),
+                              ],
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.only(right: 60.0),
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  const Text(
+                                    'Время:',
+                                    style: TextStyle(
+                                      fontSize: 12,
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                  Text(
+                                    item['time'],
+                                    style: const TextStyle(
+                                      fontSize: 12,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        );
+      },
+    );
+  }
+}
